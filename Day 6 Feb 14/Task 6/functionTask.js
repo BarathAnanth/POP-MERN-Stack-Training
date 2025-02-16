9627262d29a461e7b5bdf35b93eecc16921dbb8d
Barath Ananth <barathananthsg@gmail.com>
2025-02-16 22:20:39 +0530

    Task 6

---
+++ Day 6 Feb 14/Task 6/functionTask.js
@@ -0,0 +1,16 @@
+function log(num) {
+    console.log("Received number:", num);
+}
+log(3);
+
+function gret() {
+    return "Hello";
+}
+
+let msg = gret();
+console.log(msg);
+
+function mul(a, b) {
+    return a * b;
+}
+console.log(mul(2, 4));
